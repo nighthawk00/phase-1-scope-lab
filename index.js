e56fd918b7d5eae69c1a084fb08478195ccee1bf
Kevin <kevinlopez3292@gmail.com>
2022-11-27 14:03:45 -0500

    commit tree 2d92b32edd747fdbb9f8a2351cc9e98395aed2a8

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+    return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    window.bestCustomer = 'not bob';
+    return bestCustomer;
+}
+
+function overwriteBestCustomer() {
+    window.bestCustomer = 'maybe bob';
+    return bestCustomer;
+}
+
+function changeLeastFavoriteCustomer() {
+    const leastFavoriteCustomer = "";
+    leastFavoriteCustomer = 'Kev';
+}
